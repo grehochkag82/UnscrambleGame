841c1aede603878df276b848929cfa08e83f79d2
grehochka82 <lobodina0607@mail.ru>
2026-04-16 23:50:40 +0300

    Step 4: Add words data and game logic

--- app/src/main/java/com/lobodina/unscramblegame/ui_model/GameViewModel.kt
+++ app/src/main/java/com/lobodina/unscramblegame/ui_model/GameViewModel.kt
@@ -10,11 +10,33 @@ class GameViewModel : ViewModel() {
 
     private val _uiState = MutableStateFlow(GameUiState())
     val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+    private lateinit var currentWord: String
+    private var usedWords: MutableSet<String> = mutableSetOf()
 
     init {
         resetGame()
     }
     fun resetGame() {
-
+        usedWords.clear()
+        _uiState.value = GameUiState(
+            currentScrambledWord = pickRandomWordAndShuffle(),
+        )
     }
+    private fun shuffleCurrentWord(word: String): String {
+        val tempWord = word.toCharArray()
+        tempWord.shuffle()
+        while (String(tempWord) == word) {
+            tempWord.shuffle()
+        }
+        return String(tempWord)
+    }
+    private fun pickRandomWordAndShuffle(): String {
+        currentWord = allWords.random()
+        while (usedWords.contains(currentWord)) {
+            currentWord = allWords.random()
+        }
+        usedWords.add(currentWord)
+        return shuffleCurrentWord(currentWord)
+    }
+
 }
---
+++ app/src/main/java/com/lobodina/unscramblegame/ui_model/WordsData.kt
@@ -0,0 +1,13 @@
+package com.lobodina.unscramblegame.ui_model
+
+// Максимальное количество слов в игре
+const val MAX_NO_OF_WORDS = 10
+// Количество очков за правильный ответ
+const val SCORE_INCREASE = 20
+// Список слов для игры
+val allWords: List<String> = listOf(
+    "android", "kotlin", "compose", "jetpack", "viewmodel", "coroutine", "firebase",
+    "material", "database", "network", "retrofit", "room", "navigation", "lifecycle",
+    "dependency", "injection", "testing", "debugging", "gradle", "studio"
+)
+
